04d50fa6d19628a9503e52308d11e8d4103cd42e
Anton Rusak <4231915+arusak@users.noreply.github.com>
2022-01-14 13:19:26 +0300

    show app version

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import s from './App.module.css';
 import { StateProvider } from './state/state.context';
 import TasksScene from './scenes/TasksScene/TasksScene';
 
+const version = process.env.REACT_APP_VERSION;
+
 const App = () => {
     return (
         <div className={s.wrapper}>
             <StateProvider>
                 <TasksScene/>
             </StateProvider>
+            {version && <footer className={s.footer}>version {version}</footer>}
         </div>
     );
 };
